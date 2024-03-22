63f3666da2fdbb2d85e85b2abac7c4a91bc113bf
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-03-22 15:09:07 +0400

    Delete vars/dockerImage.groovy

--- vars/dockerImage.groovy
+++
@@ -1,7 +0,0 @@
-def dockerFunction(String param1,String param2,String param3){
-  def tmp = ${param1}
-  def mytag = tmp.substring(0, 7)
-  docker.withRegistry('https://index.docker.io/v1/', ${param2}) {
-    docker.image(${param3}).push(mytag)
-  }
-}
