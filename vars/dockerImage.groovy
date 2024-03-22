6d539d2404fb2dbae422f2c97578c3f3eec863a4
David Aristakesyan <45460144+DavoA@users.noreply.github.com>
2024-03-22 14:56:22 +0400

    Update dockerImage.groovy

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -1,7 +1,7 @@
-def dockerFunction(String param1,String param2){
-  def tmp = param1
+def dockerFunction(String param1,String param2,String param3){
+  def tmp = ${param1}
   def mytag = tmp.substring(0, 7)
-  docker.withRegistry('https://index.docker.io/v1/', param2) {
-    docker.image("parandzem/front").push(mytag)
+  docker.withRegistry('https://index.docker.io/v1/', ${param2}) {
+    docker.image(${param3}).push(mytag)
   }
 }
